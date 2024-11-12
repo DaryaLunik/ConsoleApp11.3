e7f427031c89d06ac2fb1849ee3ab7d3b6d7b70d
Kab-35-16 <Kab-35-16@KAB-32-1->
2024-11-12 14:22:59 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        public static double Fun1 (int x)
+        {
+            return (4 * Math.Pow(x, 2) + 1) / (x - 5);
+        }
+        public static double Fun2 (int x)
+        {
+            return (3 * Math.Pow(x, 2) - 2);
+        }
+        static void Main(string[] args)
+        {
+            //Задача 3. Вычисление значения функции
+            Console.Write("x=");
+            int x = int.Parse(Console.ReadLine());
+            if (x < 5)
+            {
+                double y = Fun1(x);
+            }
+            else
+            {
+                double y = Fun2(x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
